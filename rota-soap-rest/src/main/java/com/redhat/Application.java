cf49cc88d9eeacd4be66b9b416fedd3765c5d3bd
Marcelo Daniel <masales@redhat.com>
2017-07-25 20:51:28 -0300

    update route soap to rest

--- rota-soap-rest/src/main/java/com/redhat/Application.java
+++ rota-soap-rest/src/main/java/com/redhat/Application.java
@@ -1,7 +1,10 @@
 package com.redhat;
 
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
@@ -13,6 +16,13 @@ public class Application {
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+	ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/camel/*");
+	servlet.setName("CamelServlet");
+	return servlet;
     }
 
 }
