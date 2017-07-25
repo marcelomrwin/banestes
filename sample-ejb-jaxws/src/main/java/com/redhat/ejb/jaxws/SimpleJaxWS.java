7e4177f4f107ad81cde503747739153db020ccff
Marcelo Daniel <masales@redhat.com>
2017-07-25 19:05:55 -0300

    update definition

--- sample-ejb-jaxws/src/main/java/com/redhat/ejb/jaxws/SimpleJaxWS.java
+++ sample-ejb-jaxws/src/main/java/com/redhat/ejb/jaxws/SimpleJaxWS.java
@@ -11,6 +11,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.redhat.ejb.jaxws.model.User;
 
 @WebService
@@ -21,8 +24,11 @@ public class SimpleJaxWS {
     @PersistenceContext
     EntityManager entityManager;
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @WebMethod
     public User addUser(@WebParam(name = "name") String name) {
+	logger.info("received request to create user for name [" + name + "]");
 
 	User u = new User();
 	u.setName(name);
@@ -34,6 +40,8 @@ public class SimpleJaxWS {
     @SuppressWarnings("unchecked")
     @WebMethod
     public List<User> getUsers() {
+	logger.info("received request to list all users");
+
 	Query query = entityManager.createQuery("select u from User u order by u.id");
 	List<User> usersList = query.getResultList();
 	return usersList;
@@ -41,6 +49,8 @@ public class SimpleJaxWS {
 
     @WebMethod
     public void deleteUser(@WebParam(name = "id") Long id) {
+	logger.info("received request to delete user for id [" + id + "]");
+
 	User u = entityManager.find(User.class, id);
 	entityManager.remove(u);
 	entityManager.flush();
